9f4229dac452022036852c812847d3e412cbfb2e
Nilesh.M.Nair <59307717+Nilesh-M-Nair@users.noreply.github.com>
2024-10-17 13:25:39 +0530

    Update blog-banner.tsx

--- components/blog-banner.tsx
+++ components/blog-banner.tsx
@@ -41,11 +41,6 @@ export default function BlogBanner({ blogBanner }: {blogBanner : BannerProps}) {
             {blogBanner.banner_title}
           </h1>
         )}
-          {blogBanner.banner_title && (
-          <h1 className='hero-title' {...blogBanner.$?.banner_title as {}}>
-            {blogBanner.banner_title}
-          </h1>
-        )}
         {/* Duplicate block of code end here*/}
         
         {blogBanner.banner_description && (
